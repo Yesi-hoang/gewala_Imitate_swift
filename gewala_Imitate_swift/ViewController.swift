731e113838711c31fec56a6744ca484b62979336
Yesi <317447721@qq.com>
2016-04-05 09:32:43 +0800

    Initial Commit

---
+++ gewala_Imitate_swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gewala_Imitate_swift
+//
+//  Created by Yesi on 16/4/5.
+//  Copyright © 2016年 Yesi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
